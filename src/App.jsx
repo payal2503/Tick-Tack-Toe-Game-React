a92a726f801db9f2d126115497c6d3ceb81152e9
Payal <singhpayal2503@gmail.com>
2022-06-09 17:34:04 -0700

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { CIRCLE, CROSS } from '../constants';
 import { EMPTY } from '../constants';
 import './App.css';
-import Circle from './components/Circle';
+import ResultScreen from "./components/ResultScreen"
 import Square from './components/Square';
 
 const App = () => {
@@ -21,18 +21,18 @@ const App = () => {
     EMPTY,
   ])
 
-  const [winner,setWinner] = useState(null);
+  const [winner, setWinner] = useState(null);
 
   //check positions
   const checkPositions = (pos1, pos2, pos3, playerType) => {
-    if(
-      positions[pos1] === playerType && 
-      positions[pos2] === playerType && 
+    if (
+      positions[pos1] === playerType &&
+      positions[pos2] === playerType &&
       positions[pos3] === playerType
-      ){
+    ) {
       return true;
     }
-   return false;
+    return false;
   };
 
   const decision = () => {
@@ -40,35 +40,35 @@ const App = () => {
     // if 3 circles at consecutive rows
     // if 3 circles at diagnols
     //columns
-    if(checkPositions(0,1,2,CIRCLE)) return CIRCLE;
-    if(checkPositions(3,4,5,CIRCLE)) return CIRCLE;
-    if(checkPositions(6,7,8,CIRCLE)) return CIRCLE;
+    if (checkPositions(0, 1, 2, CIRCLE)) return CIRCLE;
+    if (checkPositions(3, 4, 5, CIRCLE)) return CIRCLE;
+    if (checkPositions(6, 7, 8, CIRCLE)) return CIRCLE;
     //rows
-    if(checkPositions(0,3,6,CIRCLE)) return CIRCLE;
-    if(checkPositions(1,4,7,CIRCLE)) return CIRCLE;
-    if(checkPositions(2,5,8,CIRCLE)) return CIRCLE;
+    if (checkPositions(0, 3, 6, CIRCLE)) return CIRCLE;
+    if (checkPositions(1, 4, 7, CIRCLE)) return CIRCLE;
+    if (checkPositions(2, 5, 8, CIRCLE)) return CIRCLE;
     //diagnols
-    if(checkPositions(0,4,8,CIRCLE)) return CIRCLE;
-    if(checkPositions(2,4,6,CIRCLE)) return CIRCLE;
-   
+    if (checkPositions(0, 4, 8, CIRCLE)) return CIRCLE;
+    if (checkPositions(2, 4, 6, CIRCLE)) return CIRCLE;
+
 
     // if 3 cross at consecutive columns
     // if 3 cross at consecutive rows
     // if 3 cross at diagnols
     //columns
-    if(checkPositions(0,1,2,CROSS)) return CROSS;
-    if(checkPositions(3,4,5,CROSS)) return CROSS;
-    if(checkPositions(6,7,8,CROSS)) return CROSS;
+    if (checkPositions(0, 1, 2, CROSS)) return CROSS;
+    if (checkPositions(3, 4, 5, CROSS)) return CROSS;
+    if (checkPositions(6, 7, 8, CROSS)) return CROSS;
     //rows
-    if(checkPositions(0,3,6,CROSS)) return CROSS;
-    if(checkPositions(1,4,7,CROSS)) return CROSS;
-    if(checkPositions(2,5,8,CROSS)) return CROSS;
+    if (checkPositions(0, 3, 6, CROSS)) return CROSS;
+    if (checkPositions(1, 4, 7, CROSS)) return CROSS;
+    if (checkPositions(2, 5, 8, CROSS)) return CROSS;
     //diagnols
-    if(checkPositions(0,4,8,CROSS)) return CROSS;
-    if(checkPositions(2,4,6,CROSS)) return CROSS;
+    if (checkPositions(0, 4, 8, CROSS)) return CROSS;
+    if (checkPositions(2, 4, 6, CROSS)) return CROSS;
 
     // if no match => it is a tie
-    if(positions.every(pst => pst !== EMPTY)) return EMPTY;
+    if (positions.every(pst => pst !== EMPTY)) return EMPTY;
 
     return null;
   }
@@ -84,6 +84,22 @@ const App = () => {
 
   }
 
+  const resetGame = () => {
+    setPositions([
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+      EMPTY,
+    ]);
+    setPlayer(CIRCLE);
+    setWinner(null);
+  }
+
   useEffect(() => {
     setWinner(decision);
   })
@@ -91,15 +107,19 @@ const App = () => {
   return (
     <div className="App">
       <h1 className='app_heading'>React-tick-tack-toe-game </h1>
-      <div className="game__section">
-        {
-          positions.map((pst, index)  => (         
-            <Square key={index * 55148521} position1={index} value={pst} handleMove={handleMove} />       
-          ))
-        }
-      </div>
+        
+          <div className="game__section">
+            {
+              positions.map((pst, index) => (
+                <Square key={index * 55148521} position1={index} value={pst} handleMove={handleMove} />
+              ))}
+          </div>
+        
       {/* result screen */}
-      {winner}
+     <button >{winner } Winner!</button> <br></br>
+     <button className="btn__reset" onClick={() => resetGame()}>
+        Reset Game
+      </button>
     </div>
   );
 }
--- src/components/ResultScreen/index.jsx
+++ src/components/ResultScreen/index.jsx
@@ -1,10 +1,20 @@
-import './index.css';
-const ResultScreen = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// import { CIRCLE, CROSS, EMPTY } from "../../constants";
+import "./index.css";
+
+const ResultScreen = ({ winner, resetGame }) => {
+  return (
+    <div className="result__screen">
+      <h1>
+        {winner === EMPTY && "Its a Tie"}
+        {winner === CIRCLE && "Circle Won The Game"}
+        {winner === CROSS && "Cross Won The Game"}
+      </h1>
+
+      <button className="btn__reset" onClick={() => resetGame()}>
+        Reset Game
+      </button>
+    </div>
+  );
+};
 
 export default ResultScreen;
